e365fdd863976ea09b382b6dbcdd176558644276
kyzminyarik <Yarosav_kuzmin2002@mail.ru>
2022-05-22 09:18:18 -0700

    commit1

--- sources/hash_calculation.cpp
+++ sources/hash_calculation.cpp
@@ -9,11 +9,13 @@ void stop(const int S){// обработка перырвания
 
 void get_hash(json_creator& hashes_json){
   while (in_process){
-    std::string rand_str = std::to_string(std::rand());//формирование рандомной строки
+    //формирование рандомной строки
+    std::string rand_str = std::to_string(std::rand());
     std::string hash = picosha2::hash256_hex_string(rand_str);//хэш генератор
     std::time_t timestamp(std::time(nullptr));
 
-    if (hash.substr(hash.size()-4) == "0000" && //если на конце 4 нуля то м применяем уровень логирования инфо
+    //если на конце 4 нуля то м применяем уровень логирования инфо
+    if (hash.substr(hash.size()-4) == "0000" &&
     hash.substr(hash.size()-5) != "00000"){
       BOOST_LOG_TRIVIAL(info) << "Got required hash type is found: " << hash
                               << " from string: " << rand_str;
@@ -36,7 +38,8 @@ void hash_calc(const std::string& path, size_t num_of_threads, size_t rotation){
   std::signal(SIGINT, stop);
 
   for (size_t i = 0; i < num_of_threads; ++i){
-    threads.emplace_back(get_hash, std::ref(hashes_json));//чтобы каждый поток знал с какими данными он рабоатет
+    //чтобы каждый поток знал с какими данными он рабоатет
+    threads.emplace_back(get_hash, std::ref(hashes_json));
   }
   for (auto& thread : threads){
     thread.join();//запуск потока в работу
